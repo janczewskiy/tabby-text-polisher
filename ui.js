7fec991213a72ba4a5bed2451af70d82fb1ab4cd
janczewskiy <49449494+janczewskiy@users.noreply.github.com>
2025-04-17 20:19:56 +0200

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  parent.postMessage({ pluginMessage: { type: 'request-polish' } }, '*');
+
+  onmessage = (event) => {
+    const { result, error, loading } = event.data.pluginMessage;
+    const status = document.getElementById('status');
+    const output = document.getElementById('output');
+
+    if (loading) {
+      status.textContent = 'Processing your request...';
+      output.textContent = '';
+    } else if (error) {
+      status.textContent = 'Error:';
+      output.textContent = error;
+    } else if (result) {
+      status.textContent = 'Result:';
+      output.textContent = result;
+    }
+  };
+};
